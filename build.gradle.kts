e6e88a14491fd90c549d918c3fb613b6be895071
tongxiaolong <xiaolong@muniu.tech>
2024-08-28 18:29:40 +0800

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,9 +42,9 @@ tasks {
     }
 
     signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+//        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+//        privateKey.set(System.getenv("PRIVATE_KEY"))
+//        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
     }
 
     publishPlugin {
